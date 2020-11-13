f6edd9c9b714383cc6fcc3ab2a4cc0eca47b449a
Máté Jámbor <matejambor1999@gmail.com>
2020-11-13 02:08:17 +0100

    Add files via upload

---
+++ resources/surv_model_replaces/replaces.lua
@@ -0,0 +1,99 @@
+replaces = {} --DO NOT REMOVE
+--SKINS
+--male
+
+replaces[1] = {dff="skins/1.dff", txd="skins/1.txd"}
+replaces[2] = {dff="skins/2.dff", txd="skins/2.txd"}
+replaces[7] = {dff="skins/7.dff", txd="skins/7.txd"}
+replaces[14] = {dff="skins/14.dff", txd="skins/14.txd"}
+replaces[15] = {dff="skins/15.dff", txd="skins/15.txd"}
+replaces[16] = {dff="skins/16.dff", txd="skins/16.txd"}
+replaces[17] = {dff="skins/17.dff", txd="skins/17.txd"}
+replaces[18] = {dff="skins/18.dff", txd="skins/18.txd"}
+replaces[19] = {dff="skins/19.dff", txd="skins/19.txd"}
+replaces[20] = {dff="skins/20.dff", txd="skins/20.txd"}
+replaces[21] = {dff="skins/21.dff", txd="skins/21.txd"}
+replaces[22] = {dff="skins/22.dff", txd="skins/22.txd"}
+replaces[23] = {dff="skins/23.dff", txd="skins/23.txd"}
+replaces[24] = {dff="skins/24.dff", txd="skins/24.txd"}
+replaces[25] = {dff="skins/25.dff", txd="skins/25.txd"}
+--female
+replaces[9] = {dff="skins/9.dff", txd="skins/9.txd"}
+replaces[10] = {dff="skins/10.dff", txd="skins/10.txd"}
+replaces[11] = {dff="skins/11.dff", txd="skins/11.txd"}
+replaces[12] = {dff="skins/12.dff", txd="skins/12.txd"}
+
+--VEHICLES
+replaces[433] = {dff="vehicles/barracks.dff", txd="vehicles/barracks.txd"}
+replaces[510] = {dff="vehicles/mbike.dff", txd="vehicles/mbike.txd"}
+replaces[422] = {dff="vehicles/bobcat.dff", txd="vehicles/bobcat.txd"}
+replaces[402] = {dff="vehicles/buffalo.dff", txd="vehicles/buffalo.txd"}
+replaces[482] = {dff="vehicles/burrito.dff", txd="vehicles/burrito.txd"}
+replaces[483] = {dff="vehicles/camper.dff", txd="vehicles/camper.txd"}
+replaces[542] = {dff="vehicles/clover.dff", txd="vehicles/clover.txd"}
+replaces[593] = {dff="vehicles/dodo.dff", txd="vehicles/dodo.txd"}
+replaces[490] = {dff="vehicles/fbirancher.dff", txd="vehicles/fbirancher.txd"}
+replaces[463] = {dff="vehicles/freeway.dff", txd="vehicles/freeway.txd"}
+replaces[579] = {dff="vehicles/huntley.dff", txd="vehicles/huntley.txd"}
+replaces[411] = {dff="vehicles/infi.dff", txd="vehicles/infi.txd"}
+replaces[424] = {dff="vehicles/injector.dff", txd="vehicles/injector.txd"}
+replaces[400] = {dff="vehicles/landstalker.dff", txd="vehicles/landstalker.txd"}
+replaces[487] = {dff="vehicles/maverick.dff", txd="vehicles/maverick.txd"}
+replaces[500] = {dff="vehicles/mesa.dff", txd="vehicles/mesa.txd"}
+replaces[418] = {dff="vehicles/moonbean.dff", txd="vehicles/moonbean.txd"}
+replaces[553] = {dff="vehicles/nevada.dff", txd="vehicles/nevada.txd"}
+replaces[488] = {dff="vehicles/newmav.dff", txd="vehicles/newmav.txd"}
+replaces[470] = {dff="vehicles/patriot.dff", txd="vehicles/patriot.txd"}
+replaces[603] = {dff="vehicles/phoenix.dff", txd="vehicles/phoenix.txd"}
+replaces[497] = {dff="vehicles/polmav.dff", txd="vehicles/polmav.txd"}
+replaces[471] = {dff="vehicles/quad.dff", txd="vehicles/quad.txd"}
+replaces[563] = {dff="vehicles/raindance.dff", txd="vehicles/raindance.txd"}
+replaces[489] = {dff="vehicles/rancher.dff", txd="vehicles/rancher.txd"}
+replaces[453] = {dff="vehicles/reefer.dff", txd="vehicles/reefer.txd"}
+replaces[495] = {dff="vehicles/sandking.dff", txd="vehicles/sandking.txd"}
+replaces[468] = {dff="vehicles/sanchez.dff", txd="vehicles/sanchez.txd"}
+replaces[469] = {dff="vehicles/sparrow.dff", txd="vehicles/sparrow.txd"}
+replaces[454] = {dff="vehicles/tropic.dff", txd="vehicles/tropic.txd"}
+replaces[548] = {dff="vehicles/cargobob.dff", txd="vehicles/cargobob.txd"}
+
+--BACKPACKS
+replaces[1252] = {dff="backpacks/coyote_backpack.dff", txd="backpacks/coyote_backpack_tan.txd"}
+replaces[1550] = {dff="backpacks/coyote_backpack.dff", txd="backpacks/coyote_backpack_green.txd"}
+replaces[1248] = {dff="backpacks/alice_backpack.dff", txd="backpacks/alice_backpack.txd"}
+replaces[2114] = {dff="backpacks/army_backpack.dff", txd="backpacks/army_backpack.txd"}
+replaces[1254] = {dff="backpacks/czech_backpack.dff", txd="backpacks/czech_backpack.txd"}
+replaces[1575] = {dff="backpacks/ghillie_backpack.dff", txd="backpacks/ghillie_backpack.txd"}
+replaces[1239] = {dff="backpacks/legendary_backpack.dff", txd="backpacks/legendary_backpack.txd"}
+replaces[3026] = {dff="backpacks/assault_backpack.dff", txd="backpacks/assault_backpack.txd"}
+replaces[1598] = {dff="backpacks/tourist_backpack.dff", txd="backpacks/tourist_backpack.txd"}
+
+--WEAPONS
+replaces[355] = {dff="weapons/ak47.dff", txd="weapons/ak47.txd"}
+replaces[336] = {dff="weapons/bat.dff", txd="weapons/bat.txd"}
+replaces[346] = {dff="weapons/colt45.dff", txd="weapons/colt45.txd"}
+replaces[348] = {dff="weapons/desert_eagle.dff", txd="weapons/desert_eagle.txd"}
+replaces[342] = {dff="weapons/grenade.dff", txd="weapons/grenade.txd"}
+replaces[351] = {dff="weapons/chromegun.dff", txd="weapons/chromegun.txd"}
+replaces[356] = {dff="weapons/m4.dff", txd="weapons/m4.txd"}
+replaces[353] = {dff="weapons/mp5lng.dff", txd="weapons/mp5lng.txd"}
+replaces[352] = {dff="weapons/pdw.dff", txd="weapons/pdw.txd"}
+replaces[357] = {dff="weapons/rifle.dff", txd="weapons/rifle.txd"}
+replaces[350] = {dff="weapons/sawnoff.dff", txd="weapons/sawnoff.txd"}
+replaces[349] = {dff="weapons/shotgun.dff", txd="weapons/shotgun.txd"}
+replaces[347] = {dff="weapons/silenced.dff", txd="weapons/silenced.txd"}
+replaces[358] = {dff="weapons/sniper.dff", txd="weapons/sniper.txd"}
+replaces[343] = {dff="weapons/teargas.dff", txd="weapons/teargas.txd"}
+replaces[372] = {dff="weapons/tec9.dff", txd="weapons/tec9.txd"}
+
+
+addEventHandler("onClientResourceStart", resourceRoot, function()
+		--REPLACING
+		for id, data in pairs(replaces) do
+			engineImportTXD(engineLoadTXD(data.txd), id)
+			engineReplaceModel(engineLoadDFF(data.dff), id)
+		end
+		--REMOVING EVENT HANDLER
+		removeEventHandler("onClientResourceStart", resourceRoot, getEventHandlers("onClientResourceStart", resourceRoot)[1])
+		replaces = nil
+	end
+)
